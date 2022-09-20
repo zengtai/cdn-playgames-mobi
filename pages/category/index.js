ba7b669008b8191439d74a096ded9303128d70a2
Tai Zeng <zengtai@gmail.com>
2022-09-20 16:20:19 +0800

    修改分类列表页数据逻辑

--- pages/category/index.js
+++ pages/category/index.js
@@ -16,72 +16,128 @@ import { games } from "../../data/games";
 import { categories } from "../../data/categories";
 import Banner from "../../components/Banner";
 import List from "../../components/List";
-import { getAllCategoriesWithSlug, getGamesByCategory } from "../../lib/api";
+import {
+  getAllCategoriesWithSlug,
+  getGamesByCategory,
+  getDataForCategoryList,
+} from "../../lib/api";
 import { ADS_SLOT_ID, IMAGE_PATH, IMAGE_FORMAT } from "../../lib/constants";
 import Image from "next/future/image";
 
 import { useEffect, useState } from "react";
 
-const CategoryListPage = ({ games, categories }) => {
+const CategoryListPage = ({ categories, allGames }) => {
   // console.log(`data`, games.length);
   // let currentData = games.slice();
   // let mostlyPlayed = games.slice(0, 6);
-  console.log(`categories`, categories);
+  // console.log(`categories`, categories);
+  // console.log(`games`, games);
+  // console.log(`games`, games);
+  console.log(`allGames`, allGames);
+
+  // const [games, setGames] = useState([]);
+
+  // useEffect(() => {
+  //   let tmp = [];
+  //   categories.map(async (item) => {
+  //     let games = await getGamesByCategory(item.slug);
+  //     let tmpData = {
+  //       category: { name: games[0].category.name, slug: item.slug },
+  //       games: [...games.map((i) => i.gid)],
+  //     };
+
+  //     tmp.push(tmpData);
+  //   });
+
+  //   setGames((g) => [...g, tmp]);
+  // }, [categories]);
+
   // console.log(`games`, games);
 
   return (
     <Layout>
       <h1 className="m-4 flex justify-center text-xl font-bold text-cyan-600">
-        <span>Category List</span>
+        <span>Category</span>
       </h1>
       <ul className="m-4 grid gap-2">
-        {categories.map(async (item, index) => {
-          if (index < 2) {
-            let currentData = await getGamesByCategory(item.slug, 3);
-            console.log(item.slug, currentData);
-            console.log(typeof currentData);
-          }
+        {allGames.map((item, index) => {
           return (
-            <li key={item.slug}>
-              <Link href={`/category`}>
+            <li key={item.category.slug}>
+              <Link href={`/category/${item.category.slug}`}>
                 <a className="flex justify-between rounded-lg border bg-white p-3">
                   <div className="flex items-center space-x-2">
-                    <CasualIcon />
-                    <span>Casual</span>
+                    <span>{item.category.name}</span>
                   </div>
-                  <div className="flex items-center gap-x-2">
-                    <ul className="flex -space-x-1">
-                      {currentData &&
-                        currentData
-                          .filter((item) => item.category.name === "Casual")
-                          .slice(0, 3)
-                          .map((item) => (
-                            <li key={`${item.gid}-${Math.random()}`}>
-                              <Image
-                                className="rounded-full border-2 border-white"
-                                src={
-                                  IMAGE_PATH +
-                                  IMAGE_FORMAT +
-                                  `/` +
-                                  item.gid +
-                                  `.` +
-                                  IMAGE_FORMAT
-                                }
-                                alt={item.title}
-                                width={24}
-                                height={24}
-                              />
-                            </li>
-                          ))}
-                    </ul>
+                  <div className="flex items-center space-x-2">
+                    {index < 2 ? (
+                      <ul className="flex -space-x-1">
+                        {item.games.slice(0, 3).map((game) => (
+                          <li key={`${game}-${Math.random()}`}>
+                            <Image
+                              className="rounded-full border-2 border-white"
+                              src={
+                                IMAGE_PATH +
+                                IMAGE_FORMAT +
+                                `/` +
+                                game +
+                                `.` +
+                                IMAGE_FORMAT
+                              }
+                              alt={game}
+                              width={24}
+                              height={24}
+                            />
+                          </li>
+                        ))}
+                      </ul>
+                    ) : null}
                     <span className="text-xs">
-                      {currentData && currentData.length}
-                      {"+"}
+                      {item.games.length + (index < 2 ? `+` : ``)}
                     </span>
                   </div>
                 </a>
               </Link>
             </li>
+            // <li key={item.slug}>
+            //   <Link href={`/category`}>
+            //     <a className="flex justify-between rounded-lg border bg-white p-3">
+            //       <div className="flex items-center space-x-2">
+            //         <CasualIcon />
+            //         <span>Casual</span>
+            //       </div>
+            //       <div className="flex items-center gap-x-2">
+            //         <ul className="flex -space-x-1">
+            //           {currentData &&
+            //             currentData
+            //               .filter((item) => item.category.name === "Casual")
+            //               .slice(0, 3)
+            //               .map((item) => (
+            //                 <li key={`${item.gid}-${Math.random()}`}>
+            //                   <Image
+            //                     className="rounded-full border-2 border-white"
+            //                     src={
+            //                       IMAGE_PATH +
+            //                       IMAGE_FORMAT +
+            //                       `/` +
+            //                       item.gid +
+            //                       `.` +
+            //                       IMAGE_FORMAT
+            //                     }
+            //                     alt={item.title}
+            //                     width={24}
+            //                     height={24}
+            //                   />
+            //                 </li>
+            //               ))}
+            //         </ul>
+            //         <span className="text-xs">
+            //           {currentData && currentData.length}
+            //           {"+"}
+            //         </span>
+            //       </div>
+            //     </a>
+            //   </Link>
+            // </li>
           );
         })}
       </ul>
@@ -100,20 +156,52 @@ const CategoryListPage = ({ games, categories }) => {
     </Layout>
   );
 };
-export const Head = () => <title>Category</title>;
+
 export default CategoryListPage;
 
 export const getStaticProps = async (ctx) => {
   // const currentGames = await getGamesByCategory();
   const allCategories = await getAllCategoriesWithSlug();
+  // 1.获取全部分类
+  // 2.按分类筛选游戏，截取前三个
+  // 3.push到数组
+
+  const allGames = await getDataForCategoryList();
+
+  let formatData = [];
+
+  allCategories.map((item) => {
+    let tmp = allGames.filter((game) => game.category.slug == item.slug);
+    let tmpData = {
+      category: {
+        name: tmp[0].category.name,
+        slug: item.slug,
+      },
+      games: tmp.map((i) => i.gid),
+    };
+    formatData.push(tmpData);
+  });
+
+  console.log(`formatData (`, formatData.length, `)`, formatData);
+
+  // let tmp = [];
+  // allCategories.map(async (item) => {
+  //   let games = await getGamesByCategory(item.slug, 3);
+  //   let tmpData = {
+  //     category: { name: games[0].category.name, slug: item.slug },
+  //     games: [...games.map((i) => i.slug)],
+  //   };
+
+  //   tmp.push(tmpData);
+  //   // console.log(`tmp (`, tmp.length, `)`, tmp);
+  // });
+
+  // console.log(`tmp (`, tmp.length, `)`, tmp);
 
   return {
     props: {
-      // games: allGames,
+      allGames: formatData,
       categories: allCategories,
-
-      // category: currentGames[0].category.name,
-      // categories: games,
     },
     // revalidate: 60,
   };
